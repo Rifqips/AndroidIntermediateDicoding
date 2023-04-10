1c3c6da0574911f1c3faddba6e815076b1854089
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2023-04-10 21:39:43 +0700

    done

--- app/src/main/java/com/rifqipadisiliwangi/androidintermediatedicoding/MainActivity.kt
+++ app/src/main/java/com/rifqipadisiliwangi/androidintermediatedicoding/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.rifqipadisiliwangi.androidintermediatedicoding
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.loadUrl("http://www.dicoding.com")
+
+        val webSettings = myWebView.settings
+        webSettings.javaScriptEnabled = true
     }
 }
